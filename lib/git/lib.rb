934217c3822d54e9a2c349af6ed045f2cea340b5
Matthew Scharley <matt.scharley@gmail.com>
2011-03-08 19:33:20 +1100

    Switched from single quoting commands to double quoting commands.

--- lib/git/lib.rb
+++ lib/git/lib.rb
@@ -690,7 +690,7 @@ module Git
       
       opts = [opts].flatten.map {|s| escape(s) }.join(' ')
       git_cmd = "git #{cmd} #{opts} #{redirect} 2>&1"
-      #p git_cmd
+      p git_cmd
       
       out = nil
       if chdir && (Dir.getwd != path)
@@ -722,8 +722,8 @@ module Git
     end
 
     def escape(s)
-      escaped = s.to_s.gsub(%q{'}, %q{'"'"'})
-      %Q{'#{escaped}'}
+      escaped = s.to_s.gsub(%q{"}, %q{"'"'"'})
+      %Q{"#{escaped}"}
     end
 
   end
